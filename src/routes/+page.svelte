62f7233df09faba850f141fe95277d9e04340663
dpaulos6 <itzframepvp@outlook.com>
2024-09-10 11:04:45 +0100

    complete project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,48 @@
 <script>
+	import Github from '../icons/github.svelte';
 	import Instagram from '../icons/instagram.svelte';
 	import Linkedin from '../icons/linkedin.svelte';
+	import Threads from '../icons/threads.svelte';
+	import Tiktok from '../icons/tiktok.svelte';
+	import Twitch from '../icons/twitch.svelte';
+	import X from '../icons/x.svelte';
+	import Link from './Link.svelte';
+
+	const links = [
+		{ label: 'Instagram', href: 'https://instagram.com/dpaulos6', icon: Instagram },
+		{ label: 'LinkedIn', href: 'https://linkedin.com/in/dpaulos6', icon: Linkedin },
+		{ label: 'GitHub', href: 'https://github.com/dpaulos6', icon: Github },
+		{ label: 'X', href: 'https://x.com/dpaulos6', icon: X },
+		{ label: 'Threads', href: 'https://threads.net/@dpaulos6', icon: Threads },
+		{ label: 'Twitch', href: 'https://twitch.tv/dpaulos6', icon: Twitch },
+		{ label: 'Tiktok', href: 'https://tiktok.com/@dpaulos6', icon: Tiktok }
+	];
 </script>
 
 <svelte:head>
-	<title>dpaulos6' Links</title>
+	<title>dpaulos6 links</title>
 	<meta name="description" content="All my public social medias" />
 </svelte:head>
 
-<main class="flex-1 flex justify-center items-center backdrop-blur">
-	<div class="flex flex-col gap-8 min-w-[420px] min-h-[650px] bg-white rounded-xl p-4 sm:p-6">
-		<div class="flex gap-4 justify-center items-center">
-			<img src="https://github.com/dpaulos6.png" alt="dpaulos6" class="w-16 h-16 rounded-full" />
+<main
+	class="flex flex-1 items-center justify-center overflow-x-hidden bg-black/50 px-6 py-16 backdrop-blur-md"
+>
+	<div class="flex h-fit w-fit flex-col gap-8 rounded-xl bg-black/10 p-6 transition">
+		<div class="flex items-center justify-center gap-4">
+			<img src="https://github.com/dpaulos6.png" alt="dpaulos6" class="h-20 w-20 rounded-full" />
 			<div class="flex flex-col justify-center">
-				<h1 class="text-2xl font-medium">dpaulos<span class="text-primary">6</span></h1>
-				<span class="text-base text-gray-500">Full-stack Developer</span>
+				<span class="text-2xl font-medium">
+					dpaulos<span class="text-primary">6</span>
+				</span>
+				<span class="text-base font-medium text-white/60">Full-stack Developer</span>
 				<a href="mailto:dpaulos6@outlook.com" class="text-sm text-primary">dpaulos6@outlook.com</a>
 			</div>
 		</div>
 
-		<div class="flex flex-col gap-4 px-12">
-			<a
-				href="https://instagram.com/dpaulos6"
-				class="flex items-center px-4 gap-4 w-full h-16 hover:bg-black/5 hover:-translate-y-1 hover:shadow transition rounded-lg"
-			>
-				<Instagram class="w-8 h-8" />
-				<span>Instagram</span>
-			</a>
-			<a
-				href="https://instagram.com/dpaulos6"
-				class="flex items-center px-4 gap-4 w-full h-16 hover:bg-black/5 hover:-translate-y-1 hover:shadow transition rounded-lg"
-			>
-				<Linkedin class="w-8 h-8" />
-				<span>LinkedIn</span>
-			</a>
+		<div class="flex flex-col gap-4">
+			{#each links as link}
+				<Link {link} />
+			{/each}
 		</div>
 	</div>
 </main>
---
+++ src/routes/Link.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let link;
+</script>
+
+<a
+	href={link.href}
+	target="_blank"
+	class="flex h-16 w-full items-center gap-4 rounded-lg bg-black/25 px-4 transition hover:-translate-y-1 hover:bg-black/30"
+>
+	<svelte:component this={link.icon} class="h-8 w-8" />
+	<span>{link.label}</span>
+</a>
