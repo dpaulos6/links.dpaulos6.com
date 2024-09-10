e12b1f175cf54c40ebc5aaca035ebc37076e5137
dpaulos6 <itzframepvp@outlook.com>
2024-09-10 11:21:25 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
 	import Github from '../icons/github.svelte';
 	import Instagram from '../icons/instagram.svelte';
 	import Linkedin from '../icons/linkedin.svelte';
@@ -17,6 +18,13 @@
 		{ label: 'Twitch', href: 'https://twitch.tv/dpaulos6', icon: Twitch },
 		{ label: 'Tiktok', href: 'https://tiktok.com/@dpaulos6', icon: Tiktok }
 	];
+
+	let mounted = false;
+
+	onMount(() => {
+		if (mounted) return;
+		mounted = true;
+	});
 </script>
 
 <svelte:head>
@@ -24,25 +32,28 @@
 	<meta name="description" content="All my public social medias" />
 </svelte:head>
 
-<main
-	class="flex flex-1 items-center justify-center overflow-x-hidden bg-black/50 px-6 py-16 backdrop-blur-md"
->
-	<div class="flex h-fit w-fit flex-col gap-8 rounded-xl bg-black/10 p-6 transition">
-		<div class="flex items-center justify-center gap-4">
-			<img src="https://github.com/dpaulos6.png" alt="dpaulos6" class="h-20 w-20 rounded-full" />
-			<div class="flex flex-col justify-center">
-				<span class="text-2xl font-medium">
-					dpaulos<span class="text-primary">6</span>
-				</span>
-				<span class="text-base font-medium text-white/60">Full-stack Developer</span>
-				<a href="mailto:dpaulos6@outlook.com" class="text-sm text-primary">dpaulos6@outlook.com</a>
+{#if mounted}
+	<main
+		class="flex flex-1 items-center justify-center overflow-x-hidden bg-black/50 px-6 py-16 backdrop-blur-md"
+	>
+		<div class="flex h-fit w-fit flex-col gap-8 rounded-xl bg-black/10 p-6 transition">
+			<div class="flex items-center justify-center gap-4">
+				<img src="https://github.com/dpaulos6.png" alt="dpaulos6" class="h-20 w-20 rounded-full" />
+				<div class="flex flex-col justify-center">
+					<span class="text-2xl font-medium">
+						dpaulos<span class="text-primary">6</span>
+					</span>
+					<span class="text-base font-medium text-white/60">Full-stack Developer</span>
+					<a href="mailto:dpaulos6@outlook.com" class="text-sm text-primary">dpaulos6@outlook.com</a
+					>
+				</div>
+			</div>
+
+			<div class="flex flex-col gap-4">
+				{#each links as link}
+					<Link {link} />
+				{/each}
 			</div>
 		</div>
-
-		<div class="flex flex-col gap-4">
-			{#each links as link}
-				<Link {link} />
-			{/each}
-		</div>
-	</div>
-</main>
+	</main>
+{/if}
